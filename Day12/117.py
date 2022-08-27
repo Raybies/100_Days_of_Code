25e87760ba2d8b33fbbe88a382e95e215fcc4abd
Raybies <raybies7@gmail.com>
2022-08-27 15:52:08 -0500

    updated to include a return value and notes

--- Day12/117.py
+++ Day12/117.py
@@ -1,10 +1,18 @@
-game_level = 3
+# game_level = 3
+#
+# def create_enemy():
+#     enemies = ["Skeleton", "Zombie", "Alien"]
+#     if game_level < 5:
+#         new_enemy = enemies[0]
+#         return new_enemy
+#
+# print(new_enemy)
 
-def create_enemy():
-    enemies = ["Skeleton", "Zombie", "Alien"]
-    if game_level < 5:
-        new_enemy = enemies[0]
-        return new_enemy
+enemies = 1
 
-print(new_enemy)
+def increase_enemies():
+    return enemies + 1 #returning the value without updating the variable
+    print(f"enemies inside function: {enemies}")
 
+enemies = increase_enemies() #when function is called it updates the variable enemies to be the output of the function without changing the value of the variable
+print(f"enemies outside function: {enemies}")
