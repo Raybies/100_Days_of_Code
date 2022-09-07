e6c79adcd1139330b897a91bf1d7a3506f03b857
Raybies <raybies7@gmail.com>
2022-09-07 18:37:43 -0500

    added __INIT__.PY  to Day14

--- Day14/134.py
+++ Day14/134.py
@@ -2,8 +2,12 @@
 #TODO random pick game_data
 import random
 from random import randint
+
+import sys
+sys.path.append("/Day14/")
 from 134game_data import data
-# pick a random number coorisponding to an index number in data[]
+
+# pick a random number corresponding to an index number in data[]
 pick1 = randint(0, 49)
 pick2 = randint(0, 49)
 print(random.choice(data))
